89548173e0016e20f27b4a6b3b4547bdf015630b
Smeidir <torbjsme@ntnu.no>
2024-11-11 16:36:01 +0100

    ipopt not running due to no c++

--- solver.py
+++ solver.py
@@ -6,6 +6,7 @@ import rustworkx as rx
 from rustworkx import is_connected
 from rustworkx.visualization import mpl_draw as draw_graph
 from load_data import load_graph_from_csv
+from cyipopt import minimize_ipopt
 
 class Solver():
     """
@@ -21,7 +22,7 @@ class Solver():
 
         if relaxed: 
             self.variables = self.model.continuous_var_list(len(self.graph),lb=0,ub=1, name='x')
-            #self.model.parameters.optimalitytarget =3
+            self.model.parameters.optimalitytarget =3
         else:
             self.variables = self.model.binary_var_list(len(self.graph), name='x')
         
@@ -32,7 +33,7 @@ class Solver():
                 if i != j and i> j:  
                     try:
                         graph.get_edge_data(i,j)
-                        objective -= var + var2 - 2*var*var2
+                        objective += var + var2 - 2*var*var2
                     except NoEdgeBetweenNodes:
                         pass
         self.objective = objective
@@ -60,6 +61,29 @@ class Solver():
         rx.visualization.mpl_draw(self.graph, node_color=colors, node_size=100, alpha=0.8, pos=pos)
 solver = Solver(load_graph_from_csv('data/11_nodes_links_scand.csv'), True)
 solver.solve()
+def max_cut_objective(x, graph):
+    objective = 0
+    for i in range(len(x)):
+        for j in range(i + 1, len(x)):
+            if graph.has_edge(i, j):
+                objective += x[i] + x[j] - 2 * x[i] * x[j]
+    return -objective  # Minimize the negative to maximize the original objective
 
+def max_cut_constraint(x):
+    return x - 1
+
+def solve_with_ipopt(graph):
+    x0 = [0.5] * len(graph)  # Initial guess
+    bounds = [(0, 1) for _ in range(len(graph))]
+    constraints = {'type': 'ineq', 'fun': max_cut_constraint}
+    
+    result = minimize_ipopt(fun=max_cut_objective, x0=x0, args=(graph,), bounds=bounds, constraints=constraints)
+    
+    solution_values = result.x
+    print(result.fun, solution_values)
+    return solution_values, -result.fun
+
+bitstring, objective_value = solve_with_ipopt(load_graph_from_csv('data/11_nodes_links_scand.csv'))
+print(f'Objective value: {objective_value}, Bitstring: {bitstring}')
 
    

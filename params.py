738368ba778532b7b8456ece05d0417e7f004e0c
Smeidir <torbjsme@ntnu.no>
2024-12-06 10:56:10 +0100

    graphs for vizualisation

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 graph_size = 7
 depth = 3
-supported_qaoa_variants = ['vanilla', 'multiangle'] #TODO: Recursive
+supported_qaoa_variants = ['vanilla', 'multiangle','recursive'] #TODO: Recursive
 supported_param_inits = ['uniform','gaussian']
 supported_optimizers = ['COBYLA', 'COBYQA']

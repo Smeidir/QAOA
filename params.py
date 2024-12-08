fce18606071399a8410a5ffb860d5996c5620823
Smeidir <torbjsme@ntnu.no>
2024-12-08 15:57:50 +0100

    ready for actual solstorm run with params from terminal

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 graph_size = 7
-depth = 3
+depth = 1
 supported_qaoa_variants = ['vanilla', 'multiangle','recursive']
 supported_param_inits = ['uniform','gaussian']
 supported_optimizers = ['COBYLA', 'COBYQA']

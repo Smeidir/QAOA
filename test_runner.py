305235687b6e8ffa7044b48b76e49f1f5d1b0890
Smeidir <torbjsme@ntnu.no>
2024-12-08 15:24:20 +0100

    UTF problem 5

--- test_runner.py
+++ test_runner.py
@@ -17,7 +17,7 @@ with open("email_credentials.txt", "r") as f:
 import ast
 import networkx as nx
 
-with open("test_settings.txt", "r") as f:
+with open("test_settings.txt", "r", encoding='UTF-16') as f:
     settings = ast.literal_eval(f.read().strip())
 
 
@@ -42,12 +42,12 @@ for i in range(9,4, -1):
     graphs_i, names_i = problem.get_test_graphs(i)
     graphs.append(graphs_i) #TODO: check that this works for very small values
     names.append(names_i)
-
+print(len(graphs))
 graphs = list(itertools.chain.from_iterable(graphs))
 names = list(itertools.chain.from_iterable(names))
 
 combos = [settings, graphs]
-
+print('combos', len(combos))
 
 all_combos = list(itertools.product(*combos))
 
@@ -55,13 +55,13 @@ all_combos = list(itertools.product(*combos))
 all_combos = [combo + (names[graphs.index(combo[1])],) for combo in all_combos]
 all_combos_dict = [{"parameters": combo[0], "graph": combo[1], "name": combo[2]} for combo in all_combos]
 #TODO: make dictionary
-
+print('len all_combos',len(all_combos))
 
 print('Settings:', settings)
 
 futures = [parallell_runner.remote(parameters, graph, name) for parameters, graph, name in all_combos]
 
-result_ids, unfinished = ray.wait(futures, timeout = 60*60*12, num_returns = len(graphs))
+result_ids, unfinished = ray.wait(futures, timeout = 60*60*12, num_returns = len(all_combos))
 for task in unfinished:
     ray.cancel(task)
 underway_df = pd.DataFrame(ray.get(result_ids))

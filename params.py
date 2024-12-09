e4f63b80f4f6eb6aabb42bd807ee900fbf13da91
Smeidir <torbjsme@ntnu.no>
2024-12-09 11:35:56 +0100

    can run now without errors. Added two-step parallellization to better exploit solstorm

--- params.py
+++ params.py
@@ -1,5 +1,3 @@
-graph_size = 7
-depth = 1
 supported_qaoa_variants = ['vanilla', 'multiangle','recursive']
 supported_param_inits = ['uniform','gaussian']
 supported_optimizers = ['COBYLA', 'COBYQA']

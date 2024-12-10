1ccaf202f1d023c0dac9c07f5c20017cbc92036d
Smeidir <torbjsme@ntnu.no>
2024-12-10 11:34:34 +0100

    removed superfluous recursive parameter

--- singlegraph_runner.py
+++ singlegraph_runner.py
@@ -33,7 +33,7 @@ def parallell_runner(parameters, graph,name):
     date_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
     print(f"Processing task {parameters}, {name} at time {date_time}")
     qaoa = QAOArunner(graph, simulation=True, param_initialization=parameters[0], optimizer =  parameters[1], 
-    qaoa_variant=parameters[2], warm_start=parameters[3], errors = parameters[4],depth = parameters[5], recursive_vars = parameters[6])
+    qaoa_variant=parameters[2], warm_start=parameters[3], errors = parameters[4],depth = parameters[5])
     qaoa.build_circuit()
     qaoa.run()
     solver = Solver(graph)
@@ -42,7 +42,7 @@ def parallell_runner(parameters, graph,name):
     date_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))
     print(f"Solved task {parameters}, {name} at time {date_time}. It took {end_time-timestamp} seconds.")
     return {'param_initialization': parameters[0], 'optimizer': parameters[1],'qaoa_variant': parameters[2], 'warm_start' : parameters[3], 
-    'errors':parameters[4], 'depth' : parameters[5], 'recursiv_vars':parameters[6], 'graph_size': len(graph.nodes()), 'graph_name' : name,
+    'errors':parameters[4], 'depth' : parameters[5],  'graph_size': len(graph.nodes()), 'graph_name' : name,
         'time_elapsed': qaoa.time_elapsed, 'quantum_func_evals': qaoa.fev, 'obj_func_evolution': qaoa.objective_func_vals,
         'quantum_solution':qaoa.solution, 'quantum_obj_value' : qaoa.objective_value, 
         'classic_solution' : bitstring, 'classic_value': value }

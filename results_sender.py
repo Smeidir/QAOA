2c393d35f1fc59f4aec1756838908ebb5bf0da5b
Smeidir <torbjsme@ntnu.no>
2025-03-31 13:07:36 +0200

    mails

--- results_sender.py
+++ results_sender.py
@@ -3,7 +3,7 @@ import yagmail
 
 df = pd.read_csv('results_underway.csv', on_bad_lines="skip")
 chunk_size = 1000
-num_chunks = 5
+num_chunks = 24
 
 with open("email_credentials.txt", "r") as f:
     email_password = f.read().strip()
@@ -11,6 +11,8 @@ with open("email_credentials.txt", "r") as f:
 yag = yagmail.SMTP("torbjorn.solstorm@gmail.com", email_password)
 recipient = "torbjorn.smed@gmail.com"
 subject = "Data from Python Script"
+
+df = df.iloc[-(chunk_size * num_chunks):]  # Take only the last chunk_size * num_chunks rows
 
 for i in range(num_chunks):
     chunk = df.iloc[i * chunk_size:(i + 1) * chunk_size]

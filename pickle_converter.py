388d5f35134913cb8c4ad0fe5458bb999fecba96
Smeidir <torbjsme@ntnu.no>
2025-04-09 11:27:09 +0200

    find the culprit column

--- pickle_converter.py
+++ pickle_converter.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 df = pd.read_csv("results/results_papergraph_depth_{2, 4, 6, 8, 10}.csv")
-df.drop("obj_func_evolution", axis=1, inplace=True)
-df.to_csv("results/results_papergraph_depth_{2, 4, 6, 8, 10}.csv")
+print(f"Memory usage of DataFrame: {df.memory_usage(deep=True)} bytes")
+df.to_csv("results/results_papergraph_depth_{2, 4, 6, 8, 10}_size_fixed.csv")

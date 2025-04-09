8936216d02713fcf1f3584dd7c96869c685fba3e
Smeidir <torbjsme@ntnu.no>
2025-04-09 12:52:51 +0200

    find the culprit column

--- pickle_converter.py
+++ pickle_converter.py
@@ -10,22 +10,8 @@ with open("email_credentials.txt", "r") as f:
 yag = yagmail.SMTP("torbjorn.solstorm@gmail.com", email_password)
 recipient = "torbjorn.smed@gmail.com"
 subject = "Data from Python Script"
-body = 'Solstorm run -papergraph - results_papergraph_depth_{2, 4, 6}.csv'
-attachment = 'results/results_papergraph_depth_{2, 4, 6}.csv'
-
-yag.send( subject=subject, contents=body, attachments=attachment)
-print("Email sent successfully!")
-yag.close()
-
-df = pd.read_csv("results/results_papergraph_depth_{8, 10}.csv")
-df = df.drop(columns=["obj_func_evals"], errors="ignore")
-df.to_csv("results/results_papergraph_depth_{8, 10}.csv")
-
-yag = yagmail.SMTP("torbjorn.solstorm@gmail.com", email_password)
-recipient = "torbjorn.smed@gmail.com"
-subject = "Data from Python Script"
-body = 'Solstorm run -papergraph - results_papergraph_depth_{8, 10}.csv'
-attachment = 'results/results_papergraph_depth_{8, 10}.csv'
+body = 'Solstorm run -papergraph - results_papergraph_depth_{2, 4, 6, 8, 10}.csv'
+attachment = 'results/results_papergraph_depth_{2, 4, 6, 8, 10}.csv'
 
 yag.send( subject=subject, contents=body, attachments=attachment)
 print("Email sent successfully!")

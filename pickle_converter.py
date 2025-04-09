a3eb5bbcf20a1941d0e73a37aee11a307dd43c68
Smeidir <torbjsme@ntnu.no>
2025-04-09 12:51:39 +0200

    find the culprit column

--- pickle_converter.py
+++ pickle_converter.py
@@ -5,9 +5,17 @@ import yagmail
 with open("email_credentials.txt", "r") as f:
     email_password = f.read().strip()
 
-df = pd.read_csv("results/results_papergraph_depth_{2, 4, 6}.csv")
-df = df.drop(columns=["obj_func_evals"], errors="ignore")
-df.to_csv("results/results_papergraph_depth_{2, 4, 6}.csv")
+
+
+yag = yagmail.SMTP("torbjorn.solstorm@gmail.com", email_password)
+recipient = "torbjorn.smed@gmail.com"
+subject = "Data from Python Script"
+body = 'Solstorm run -papergraph - results_papergraph_depth_{2, 4, 6}.csv'
+attachment = 'results/results_papergraph_depth_{2, 4, 6}.csv'
+
+yag.send( subject=subject, contents=body, attachments=attachment)
+print("Email sent successfully!")
+yag.close()
 
 df = pd.read_csv("results/results_papergraph_depth_{8, 10}.csv")
 df = df.drop(columns=["obj_func_evals"], errors="ignore")
@@ -16,18 +24,8 @@ df.to_csv("results/results_papergraph_depth_{8, 10}.csv")
 yag = yagmail.SMTP("torbjorn.solstorm@gmail.com", email_password)
 recipient = "torbjorn.smed@gmail.com"
 subject = "Data from Python Script"
-body = f'Solstorm run -papergraph - results_papergraph_depth_{2, 4, 6}.csv'
-attachment = f'results/results_papergraph_depth_{2, 4, 6}.csv'
-
-yag.send( subject=subject, contents=body, attachments=attachment)
-print("Email sent successfully!")
-yag.close()
-
-yag = yagmail.SMTP("torbjorn.solstorm@gmail.com", email_password)
-recipient = "torbjorn.smed@gmail.com"
-subject = "Data from Python Script"
-body = f'Solstorm run -papergraph - results_papergraph_depth_{8, 10}.csv'
-attachment = f'results/results_papergraph_depth_{8, 10}.csv'
+body = 'Solstorm run -papergraph - results_papergraph_depth_{8, 10}.csv'
+attachment = 'results/results_papergraph_depth_{8, 10}.csv'
 
 yag.send( subject=subject, contents=body, attachments=attachment)
 print("Email sent successfully!")

20549287792284aa6ffcd8a4063da6f591aa2ab1
Smeidir <torbjsme@ntnu.no>
2025-04-09 10:18:17 +0200

    find the culprit column

--- pickle_converter.py
+++ pickle_converter.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('results/results_papergraph_depth_{2, 4, 6, 8, 10}.csv')
-df.to_pickle('results/results_papergraph_depth_{2, 4, 6, 8, 10}.pkl')
-print(f"Pickle file size: {df.memory_usage(deep=True).sum()} bytes")
+
+df_subset = pd.read_csv("path/to/your/file.csv", usecols=["obj_func_evolution"])
+print(df_subset.memory_usage(deep=True))

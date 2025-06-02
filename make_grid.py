a379397d2f25d0a56f67f6e13ed51a630226e381
Smeidir <torbjorn.smed@gmail.com>
2025-06-02 19:44:21 +0200

    reruns solstorm

--- make_grid.py
+++ make_grid.py
@@ -17,16 +17,16 @@ settings = {
     "backend_mode":        ["noisy_sampling"],              # or ['noisy_sampling']
     "qaoa_variant":        ["multiangle"],
     "param_initialization":["gaussian"],
-    "depth":               [1,4,7],
+    "depth":               [7],
     "warm_start":          [True],
-    "hamming_dist":        [1],
+    "hamming_dist":        [3],
     "problem_type":        ["minvertexcover"]
 }
 
 graph_paths = json.load(open("graph_paths.json"))   # {'paper1_0': '/scratch/…'}
 
 # Filter out specific graphs that should be excluded
-graph_paths = {k: v for k, v in graph_paths.items() if k not in ["paper1_0", "paper1_2"]}
+graph_paths = {k: v for k, v in graph_paths.items() if k not in ["paper1_0", "paper1_1","paper1_2"]}
 
 
 # ----------------------------------------------------------------------
